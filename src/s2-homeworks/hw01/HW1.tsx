7635ce7b131ae932c6a7082612fa7788fa5832dc
sli9 <sil9gosy@mail.ru>
2024-04-21 22:17:34 +0300

    resolved with tasks styled message and friend message

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -14,17 +14,32 @@ import avatar from './avatar.png'
 * */
 
 // нужно создать правильный тип вместо any
-export type MessageType = any
+export type MessageType = {
+    id: number
+    user: {
+        avatar: string
+        name: string
+    }
+    message: {
+        text: string
+        time: string
+    }
+}
 
 // структуру объекта не менять
 export const message0: MessageType = {
     id: 0,
     user: {
         avatar: avatar, // можно менять
-        name: 'Some Name',  // можно менять
+        name: 'Ivan',  // можно менять
     },
     message: {
-        text: 'some textsome textsome textsome textsome textsome textsome text', // можно менять
+        text: 'Hello, she didn’t do anything and rested all day, how are you?' +
+            'Hello, she didn’t do anything and rested all day, how are you?' +
+            'Hello, she didn’t do anything and rested all day, how are you?' +
+            'Hello, she didn’t do anything and rested all day, how are you?' +
+            'Hello, she didn’t do anything and rested all day, how are you?' +
+            'Hello, she didn’t do anything and rested all day, how are you?', // можно менять
         time: '22:00', // можно менять
     },
 }
@@ -32,10 +47,10 @@ export const friendMessage0: MessageType = {
     id: 100,
     user: {
         avatar: avatar, // можно менять
-        name: 'Friend Name', // можно менять
+        name: 'Ivan', // можно менять
     },
     message: {
-        text: 'зеркальное сообщение для тренировки css', // можно менять
+        text: 'Hello, how are you, what did you do yesterday?', // можно менять
         time: '22:00', // можно менять
     },
 }
@@ -52,7 +67,7 @@ const HW1 = () => {
                 </div>
 
                 {/*для автоматической проверки дз (не менять)*/}
-                <MessageSender M={Message} />
+                {/*<MessageSender M={Message} />*/}
             </div>
         </div>
     )
